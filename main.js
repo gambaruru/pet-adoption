630dae1a3d1a72db265c11bcb7a0d1b9fe8a79ba
gambaruru <gambaruru@gmail.com>
2026-01-10 19:18:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 async function start() {
-    const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
-    const weatherData = await weatherPromise.json()
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+  const weatherData = await weatherPromise.json()
 
-    const ourTemperature = weatherData.properties.periods[0].temperature
+  const ourTemperature = weatherData.properties.periods[0].temperature
+  document.querySelector("#temperature-output").textContent = ourTemperature
 
-    console.log(ourTemperature)
 }
 
 start()
+
+async function petsArea() {
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petsData = await petsPromise.json()
+  petsData.forEach(pet => {
+    console.log(pet.species)
+  })
+}
+
+petsArea()
